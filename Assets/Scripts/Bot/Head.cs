3165575ef99ffea62c226fdeb5a88e18f01fd4bb
William Hammond <william.t.hammond@gmail.com>
2022-01-30 10:34:24 -0500

    Fire from cannon head

--- Assets/Scripts/Bot/Head.cs
+++ Assets/Scripts/Bot/Head.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Head : BodyPart
 {
-    // Start is called before the first frame update
+    [SerializeField] private Weapon _weapon;
+
+    private bool _isFirstFrame;
+
     public void Start()
     {
         base.Start();
+        _isFirstFrame = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // We need this to prevent a race condition. If we start the coroutine
+        // in start, it's possible that a bot takes damage before it's summed it's
+        // total health using it's body parts.
+        if (_isFirstFrame)
+        {
+            StartCoroutine(Attack());
+            _isFirstFrame = false;
+        }
+
+        if (state == BodyPartState.DESTROYED || state == BodyPartState.GAME_OVER)
+        {
+            StopCoroutine(Attack());
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        while (hitpoints > 0 && _weapon && state != BodyPartState.DESTROYED && state != BodyPartState.GAME_OVER)
+        {
+            _weapon.Fire(this, botController.target, botController.childrenColliders);
+            yield return new WaitForSeconds(_weapon.attackRate);
+        }
     }
 }
--- Assets/Scripts/Bot/RangedWeapon.cs
+++ Assets/Scripts/Bot/RangedWeapon.cs
@@ -9,6 +9,8 @@ namespace DefaultNamespace
 
         protected override void FireEffect(BodyPart target, Vector3 position, Collider[] ignored)
         {
+            // Hack to line up with cannon nozzle
+            position = new Vector3(position.x, position.y + 6.0f, position.z);
             Transform projectileTransform = Instantiate(projectile, position, Quaternion.identity);
             foreach (Collider collider in ignored)
             {
