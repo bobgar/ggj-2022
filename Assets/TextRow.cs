8f03ac06e1db65b0fba0cd1870b2a59a4e408aac
awilkinson <AndrewWilkinson88@gmail.com>
2022-01-30 11:41:01 -0500

    RoundEndScene Animated and Designed

---
+++ Assets/TextRow.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextRow : MonoBehaviour
+{
+    public Text expenseText;
+    public Text numberText;
+}
